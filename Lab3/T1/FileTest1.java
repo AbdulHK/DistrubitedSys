ecc3b96ba8b57c5a5c3d7c4ca6cc7ed808daa475
Abdulellah Hakim <D12122837@mydit.ie>
2016-10-01 12:18:24 +0100

    lab3

---
+++ Lab3/T1/FileTest1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class FileTest1
+{
+	public static void main(String[] args) throws IOException
+	{
+		PrintWriter output =
+		  	new PrintWriter(new File("test1.txt"));
+		output.println("Single line of text! hello");
+		System.lineSeparator();
+		output.print("new line");
+		output.close();
+	}
+}
